56c1ead935c3a7560c710e14e62cd29464f524b6
dsh971 <hdavidsizhen@gmail.com>
2014-12-22 20:08:18 -0800

    Initial Commit

---
+++ Sound App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sound App
+//
+//  Created by David on 12/22/14.
+//  Copyright (c) 2014 David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
